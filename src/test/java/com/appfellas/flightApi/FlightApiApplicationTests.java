c9b965593dc56cb04791bc6002dd055c079c6d8d
Ahmet Yalcinkaya <ahmet.yalcinkaya55@gmail.com>
2024-09-17 16:41:45 +0300

    spring initialized

---
+++ src/test/java/com/appfellas/flightApi/FlightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appfellas.flightApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
